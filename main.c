72493465c63f789f2cb29d2c5eef6a8bdac00bb3
vcollado <necseron@gmail.com>
2018-07-26 20:52:32 +0200

    Consumes add and remove from main

--- main.c
+++ main.c
@@ -2,11 +2,19 @@
 #include "./tasks/taskHandler.h"
 #include "./tasks/task.h"
 
-main()
+int main()
 {
   TaskHandler taskHandler = NewTaskHandler();
-  Task task1 = NewTask("foo", "this is the first task");
-  AddTask(&taskHandler, task1, 0);
+
+  int task1Id = 0;
+  Task *task1 = NewTask(task1Id, "foo", "this is the first task");
+  int task2Id = 1;
+  Task *task2 = NewTask(task2Id, "baz", "this is the secord task");
+
+  AddTask(&taskHandler, task1);
+  AddTask(&taskHandler, task2);
+
+  RemoveTask(&taskHandler, task1Id);
 
   return 0;
 }
